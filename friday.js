56f4467995203dbfeae7e84a38c12251bad61eeb
Shakil Ibne Shaikh <kingshakil55@gmail.com>
2020-12-31 19:49:58 -0330

    Update friday.js

--- friday.js
+++ friday.js
@@ -1,38 +1,53 @@
-body{
-	background-image: url('img_bg.gif');
-	background-repeat: no-repeat;
-	background-attachment: fixed;
-	background-size: cover;
+
+function run(){
+	var width = 0;
+	openTab(event, 'home')
+    var id = setInterval(frame, 1200);
+	var x = document.getElementById("bgm");
+	function frame()
+	{
+		if(width==10)
+		{
+			x.volume = 0.5;
+			x.loop = true;
+			x.load();
+			clearInterval(id);
+			x.play();
+		}
+		else
+		{
+			width+=1;
+			console.log(width);
+		}
+	}
 }
 
-#mythumb{
-	position: fixed;
-	right:0;
-	bottom:0;
+function jump(st){
+	//pass
+	window.location = st;
 }
 
-.content{
-	background: rgba(0,0,0,0.5);
-	color: #f1f1f1;
-}
-
-.tabi{
-	background: rgba(0,0,0,0.5);
-	color: #f1f1f1;
-	width:100%;
-	padding: 20px;
-}
-
-.tablinks{
-	background: rgba(0,0,0,0.5);
-	color: #f1f1f1;
-}
-.tabcontent {
-  display: none;
-  padding: 6px 12px;
-  border-top: none;
-}
-
-.button:hover{
-	background-color: black;
+function openTab(evt, tabName) {
+	var ggwp = document.getElementById('greeter');
+	console.log(tabName)
+  var i, tabcontent, tablinks;
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+  tablinks = document.getElementsByClassName("tablinks");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+  document.getElementById(tabName).style.display = "block";
+  evt.currentTarget.className += " active";
+  
+  if(tabName=='home'){
+	  ggwp.innerHTML = "<h2>Welcome</h2>";
+  }else if(tabName=='about'){
+	  ggwp.innerHTML = "<h2>About Me</h2>";
+  }
+  else if(tabName=='projects'){
+	  ggwp.innerHTML = "<h2>My Projects</h2>";
+  }
 }
